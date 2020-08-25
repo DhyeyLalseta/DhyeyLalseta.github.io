45ee822b23c6dfadf969a73c65260e4d7b92c5b3
dhyey <dhyeylalseta@outlook.com>
2020-08-25 16:54:04 -0600

    :dizzy: aesthetic changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
-window.addEventListener('load', (event) => {
+window.addEventListener('load', () => {
     console.log("hey :) i know the page looks really boring rn but i'm constantly working on making it better. if you have any suggestions feel free to email me or message me on linkedin, the direct links are within the footer. thank you for checking out my page!");
 
-
     document.getElementById('nav-button').addEventListener('click', () => {
-        console.log("my apologies this button doesn't do anything yet ;(, but it will soon. one day i will hopefully understand how frontend works. i'm better at python anyways smh")
-    })
+        console.log("my apologies this button doesn't do anything yet ;(, but it will soon. one day i will hopefully understand how frontend works. i'm better at python anyways smh");
+    });
+
+    const typed = new Typed("#typed", {
+        strings: ["", "software engineer", "software e...", "software engineer"],
+        cursorChar: '',
+        typeSpeed: 100,
+        backSpeed: 300,
+    });
+
+    const dottyBoiImg = document.querySelector(".dotty-boi-img");
+    new BreathingHalftone(dottyBoiImg, {
+        dotSize: 1 / 160,
+        dotSizeThreshold: 0.01,
+        isAdditive: true,
+        initVelocity: 0.7,
+        oscPeriod: 2,
+        oscAmplitude: 0.2,
+        channels: ['green', 'red', 'lum'],
+        friction: 0.2,
+        hoverDiameter: 0.4,
+        hoverForce: 0.01,
+        activeDiameter: 0.5,
+        activeForce: 0.03
+    });
 });
