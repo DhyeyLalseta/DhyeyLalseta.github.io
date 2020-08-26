75bd49e26f9027f55818448fdb5fe1e78f0ddd92
dhyey <dhyeylalseta@outlook.com>
2020-08-26 17:56:47 -0600

    :beers: shoutouts

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
     console.log("hey :) i know the page looks really boring rn but i'm constantly working on making it better. if you have any suggestions feel free to email me or message me on linkedin, the direct links are within the footer. thank you for checking out my page!");
-
+    console.log("also shout out to aradhyab.me for giving me copy inspiration for my website.");
     document.getElementById('nav-button').addEventListener('click', () => {
         console.log("my apologies this button doesn't do anything yet ;(, but it will soon. one day i will hopefully understand how frontend works. i'm better at python anyways smh");
     });
